3acefe9601d77ea3825f42cd967bbba57849315d
tomcorey26 <tomcorey26@my.uri.edu>
2020-11-26 00:35:52 -0500

    out of bounds test

--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -24,3 +24,26 @@ test('Expect test input to be expected output', async () => {
   expect(rover2.y).toBe(1);
   expect(rover2.dir).toBe('E');
 });
+
+test('Expect rover not to go out of bounds', async () => {
+  let { exploreGrid } = new Grid(5, 5);
+  const rovers: MarsRover[] = createRovers([
+    ['foo'],
+    ['0', '0', 'S'],
+    ['MRM'],
+    ['5', '5', 'N'],
+    ['MRM'],
+  ]);
+  const rover1 = rovers[0];
+  await exploreGrid(rover1, true);
+
+  expect(rover1.x).toBe(0);
+  expect(rover1.y).toBe(0);
+  expect(rover1.dir).toBe('W');
+
+  const rover2 = rovers[1];
+  await exploreGrid(rover2, true);
+  expect(rover2.x).toBe(5);
+  expect(rover2.y).toBe(5);
+  expect(rover2.dir).toBe('E');
+});
